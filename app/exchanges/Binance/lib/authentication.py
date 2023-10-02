45944ab92715ab4b6de84a6f2d51de659dfce414
yankochur <mr.uanby@mail.ru>
2023-10-02 19:28:00 +0300

    API class realisation

--- app/exchanges/Binance/lib/authentication.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-import hmac
-import hashlib
-import time
-
-
-def hmac_connection(API_SECRET, payload):
-    signature = hmac.new(API_SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
-    return signature.hexdigest()
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 # from app.__init__ import create_app
-from app.exchanges.Binance.lib import base
+from app.exchanges.Binance import api
+
+connection = api.API()
 
 if __name__ == '__main__':
     # create_app().run(debug=True)
     # base.ws.run_forever()
     # base.binance_balance()
-    print(base.account_data)
+    connection.hmac_connection()
