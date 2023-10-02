918123d2190a05513760e70dd35475fd3dc5fa83
yankochur <mr.uanby@mail.ru>
2023-10-02 19:28:31 +0300

    -

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect
 
 main = Blueprint("main", __name__, template_folder="templates")
 
+
 @main.route('/')
 def index_html():
     return render_template('main/index.html')
