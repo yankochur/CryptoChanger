c58ce7e7591552a22fe4264028ed76d68eb86350
yankochur <mr.uanby@mail.ru>
2023-10-02 21:43:02 +0300

    correction in the run.py

--- run.py
+++ run.py
@@ -4,7 +4,4 @@ from app.exchanges.Binance import api
 connection = api.API()
 
 if __name__ == '__main__':
-    # create_app().run(debug=True)
-    # base.ws.run_forever()
-    # base.binance_balance()
     connection.hmac_connection()
