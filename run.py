18cc85bb61b33c748f94457e308edcfaed6ce7a9
yankochur <mr.uanby@mail.ru>
2023-10-09 17:33:28 +0300

    snake_case in this dir

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # from app.__init__ import create_app
-from app.exchanges.Binance import api as binance_api
-from app.exchanges.ByBit import api as bybit_api
+from app.exchanges.binance import api as binance_api
+from app.exchanges.bybit import api as bybit_api
 
 
-BINANCE_CONNECTION = binance_api.API()
-BYBIT_CONNECTION = bybit_api.API()
+BINANCE_CONNECTION = binance_api.BinanceAPI()
+BYBIT_CONNECTION = bybit_api.ByBitAPI()
 
 
 if __name__ == '__main__':
