a8e545fdfde4a79cc0a0d1b684ff53efdeabcdef
yankochur <mr.uanby@mail.ru>
2023-12-15 17:38:37 +0300

    Userlogin

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,9 @@
-class UserLogin():
-    def fromDB(self, user_id, db):
-        self.__user = db.getUser(user_id)
-        return self
+from pydantic import BaseModel, EmailStr, field_validator
 
-    def create(self, user):
-        self.__user = user
-        return self
+from app.main.utils import Hasher
 
+
+class UserLogin:
     def is_authenticated(self):
         return True
 
@@ -18,3 +15,13 @@ class UserLogin():
 
     def get_id(self):
         return str(self.__user['id'])
+
+
+class UserLoginForm(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator('password')
+    @classmethod
+    def _hash_password(cls, v: str) -> str:
+        return Hasher.get_password_hash(v)
