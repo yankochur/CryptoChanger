8a7ed19e7d4942d790d2eb6b5a94baa49feb1287
yankochur <mr.uanby@mail.ru>
2023-12-20 01:21:04 +0300

    migrate user app and db registration to user.py

--- app/flaskapp.py
+++ app/flaskapp.py
@@ -1,35 +1,8 @@
 from flask import Flask
 from app.main.views import main as main_template
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from app.models.user import app, db
 import bcrypt
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@127.0.0.1:5432/postgres'
-app.config['SECRET_KEY'] = '3c4dc378e50242e3b4b5802952566fd519466210'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(80), nullable=False)
-
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-    def check_password(self, password):
-        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
 
 
 def create_app():
